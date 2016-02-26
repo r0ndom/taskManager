455c909a87117809657334ae5deb46325d6d3a1c
Mednikov <Mednikov>
2016-02-26 17:14:21 +0200

    added configs

--- src/main/java/com/pb/task/manager/config/SecurityConfig.java
+++ src/main/java/com/pb/task/manager/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.pb.task.manager.config;
 
+import com.pb.task.manager.service.security.SimpleAuthenticationSuccessHandler;
+import com.pb.task.manager.service.security.UserAuthenticationProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 /**
  * Created by Mednikov on 26.02.2016.
@@ -14,33 +19,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-                // Spring Security should completely ignore URLs starting with /resources/
-                .antMatchers("/resources/**");
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        return new UserAuthenticationProvider();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new SimpleAuthenticationSuccessHandler();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/public/**").permitAll()
-                .anyRequest().hasRole("USER")
-                .and()
-                        // Possibly more configuration ...
-                .formLogin() // enable form based log in
-                        // set permitAll for all URLs associated with Form Login
-                .permitAll();
+        http.authorizeRequests().antMatchers("/app/tasks/**").
+                access("hasAnyRole('ROLE_ADMIN', 'ROLE_DEVELOPER', 'ROLE_TESTER', 'ROLE_MANAGER')");
+        http.authorizeRequests().antMatchers("/app/users/**").access("hasRole('ROLE_CONTROL')");
+        http.formLogin().loginPage("/login").usernameParameter("username").passwordParameter("password").
+                defaultSuccessUrl("/app/tasks/").failureUrl("/login?error").successHandler(authenticationSuccessHandler());
+        http.logout().logoutSuccessUrl("/login?logout");
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                // enable in memory based authentication with a user named "user" and "admin"
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER").and()
-                .withUser("admin").password("password").roles("USER", "ADMIN");
+        auth.authenticationProvider(authenticationProvider());
     }
 
-    // Possibly more overridden methods ...
 }
