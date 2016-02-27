c4d05c2225727ab6feca5e1ec1391a7e1acb80ea
r0ndom <r0ndom@yahoo.com>
2016-02-27 19:46:09 +0200

    revert changes

--- src/main/java/com/pb/task/manager/config/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-//package com.pb.task.manager.config;
-//
-//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-//
-//public class SecurityInitializer
-//      extends AbstractSecurityWebApplicationInitializer {
-//}
